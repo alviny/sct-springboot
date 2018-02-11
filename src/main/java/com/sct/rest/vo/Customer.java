341051ad5c3108a12a81ab62c3dc4e78ba5f9523
alvin.yap <alvin.yap@gmail.com>
2018-02-11 08:49:20 -0500

    Update the customer vo

--- src/main/java/com/sct/rest/vo/Customer.java
+++ src/main/java/com/sct/rest/vo/Customer.java
@@ -1,14 +1,14 @@
 package com.sct.rest.vo;
 
 public class Customer {
-	private Long customerId;
+	private Long id;
 	private String firstName;
 	private String lastName;
-	public Long getCustomerId() {
-		return customerId;
+	public Long getId() {
+		return id;
 	}
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public String getFirstName() {
 		return firstName;
